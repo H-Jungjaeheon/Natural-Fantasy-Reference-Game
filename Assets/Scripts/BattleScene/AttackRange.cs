a38bf7903e19aaefafe4dad0a91e7ae219cd4345
H-Jungjaeheon <jjhking222@naver.com>
2022-08-05 18:04:26 +0900

    BasicAttackSystem 70%Make
    BasicAttackSystem 70%Make

---
+++ Assets/Scripts/BattleScene/AttackRange.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackRange : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("이 공격 범위를 가진 해당 유닛 종류")]
+    private bool isPlayer;
+
+    [SerializeField]
+    [Tooltip("이 공격 범위를 가진 해당 유닛 오브젝트")]
+    private GameObject unitObj;
+
+    [SerializeField]
+    [Tooltip("공격 범위 콜라이더")]
+    private Collider2D attackRangeCollider;
+
+    private void OnTriggerEnter2D(Collider2D collision) //적이 범위에 들어올 시
+    {
+        if (isPlayer && collision.gameObject.CompareTag("Enemy"))
+        {
+            unitObj.GetComponent<Player>().rangeInEnemy.Add(collision.gameObject);
+        }
+        else if(isPlayer == false && collision.gameObject.CompareTag("Player"))
+        {
+            //unitObj.GetComponent<Enemy>().
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) //적이 범위에 나갈 시
+    {
+        if (isPlayer && collision.gameObject.CompareTag("Enemy"))
+        {
+            unitObj.GetComponent<Player>().rangeInEnemy.Remove(collision.gameObject);
+        }
+        else if(isPlayer == false && collision.gameObject.CompareTag("Player"))
+        {
+            //unitObj.GetComponent<Enemy>().
+        }
+    }
+}
--- Assets/Scripts/BattleScene/Player.cs
+++ Assets/Scripts/BattleScene/Player.cs
@@ -43,6 +43,12 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float jumpPower_F;
 
+    [Tooltip("현재 공격 범위에 존재하는 적")]
+    public List<GameObject> rangeInEnemy = new List<GameObject>();
+
+    private bool isComplete;
+    private int nowAttackCount_I;
+
     [HideInInspector]
     public bool isJumping;
     #endregion
@@ -124,18 +130,18 @@ public class Player : MonoBehaviour
         UISetting();
         Jump();
     }
-    private void StartSetting() //초기 세팅 (공통)
+    private void StartSetting() //초기 세팅 (일부 공통)
     {
         Cam = Camera.main;
         rigid = gameObject.GetComponent<Rigidbody2D>();
-        isWaiting = true;
         startPos_Vector = transform.position;
         maxActionCoolTime = 3.5f - GameManager.Instance.ReduceCoolTimeLevel * 0.5f;
         MaxHp_F = 25 + GameManager.Instance.MaxHpUpgradeLevel * 5;
         Hp_F = MaxHp_F;
         MaxEnergy_I = 15 + GameManager.Instance.MaxEnergyUpgradeLevel * 5;
         Energy_I = MaxEnergy_I;
-        Damage_I = 2 + GameManager.Instance.DamageUpgradeLevel;
+        Damage_I = 1 + GameManager.Instance.DamageUpgradeLevel;
+        nowAttackCount_I = 1;
         BattleSceneManager.Instance.PlayerCharacterPos = transform.position;
     }
     private void UISetting() //대기시간 및 UI세팅 (공통)
@@ -200,25 +206,92 @@ public class Player : MonoBehaviour
     {
         isAttacking = true;
         ActionButtonsSetActive(false);
-        StartCoroutine(CloseAttack());
+        StartCoroutine(GoToAttack());
     }
 
-    IEnumerator CloseAttack()
+    IEnumerator GoToAttack()
     {
-        Vector3 Movetransform = new Vector3(Speed_F * Time.deltaTime, 0, 0);
-        print(BattleSceneManager.Instance.EnemyCharacterPos.x);
-        while (transform.position.x < BattleSceneManager.Instance.EnemyCharacterPos.x)
+        Vector3 Movetransform = new Vector3(Speed_F * Time.deltaTime, 0, 0); //이동을 위해 더해줄 연산
+        Vector3 Targettransform = new Vector3(BattleSceneManager.Instance.EnemyCharacterPos.x - 5, transform.position.y); //목표 위치
+
+        while (transform.position.x < Targettransform.x) //이동중
         {
             transform.position += Movetransform;
             yield return null;
         }
-        transform.position = new Vector2(BattleSceneManager.Instance.EnemyCharacterPos.x, transform.position.y);
-        yield return new WaitForSeconds(4);
+        transform.position = Targettransform; //이동 완료
+
+        StartCoroutine(EnemyAttack(false, nowAttackCount_I, 0.3f)); //첫번째 공격
+    }
+
+    IEnumerator EnemyAttack(bool isLastAttack, int nowAttackCount_I, float attacktimelimit_f) //3연공 재귀로 구현
+    {
+        isComplete = false;
+
+        if (rangeInEnemy[0] != null)
+        {
+            print($"공격 {nowAttackCount_I}"); //공격 실행(애니메이션)
+            for (int nowIndex = 0; nowIndex < rangeInEnemy.Count; nowIndex++)
+            {
+                if (rangeInEnemy[nowIndex] != null)
+                {
+                    rangeInEnemy[nowIndex].GetComponent<Enemy>().hp -= Damage_I;
+                }
+            }
+        }
+
+        if (isLastAttack == false)
+        {
+            float nowattacktime_f = 0;
+            print("눌러");
+            while (attacktimelimit_f > nowattacktime_f) //기본공격 타이밍 계산
+            {
+                nowattacktime_f += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    isComplete = true;
+                }
+                yield return null;
+            }
+
+            if (isComplete)
+            {
+                switch (nowAttackCount_I)
+                {
+                    case 1:
+                        yield return new WaitForSeconds(0.1f);
+                        nowAttackCount_I++;
+                        StartCoroutine(EnemyAttack(false, nowAttackCount_I, 0.4f)); //두번째 공격
+                        break;
+                    case 2:
+                        yield return new WaitForSeconds(0.5f);
+                        nowAttackCount_I++;
+                        StartCoroutine(EnemyAttack(true, nowAttackCount_I, 0.4f)); //세번째 공격
+                        break;
+                }
+            }
+            else //돌아가기
+            {
+                StartCoroutine(Return());
+            }
+        }
+        else //돌아가기
+        {
+            yield return new WaitForSeconds(0.5f);
+            StartCoroutine(Return());
+        }
+    }
+
+    IEnumerator Return()
+    {
+        Vector3 Movetransform = new Vector3(Speed_F * Time.deltaTime, 0, 0); //이동을 위해 더해줄 연산
+        transform.rotation = Quaternion.Euler(0, 180, 0);
         while (transform.position.x > startPos_Vector.x)
         {
             transform.position -= Movetransform;
             yield return null;
         }
+        transform.rotation = Quaternion.identity;
         transform.position = startPos_Vector;
         isAttacking = false;
         WaitingTimeStart();
