1c31718cdebe93a454eac3aef826314ed7549dc8
H-JungJaeHeon <jjhking222@naver.com>
2022-09-26 02:40:40 +0900

    SomeFixed
    SomeFixed

--- Assets/Scripts/BattleScene/Player/Player.cs
+++ Assets/Scripts/BattleScene/Player/Player.cs
@@ -23,7 +23,7 @@ public class Player : BasicUnitScript
 
     private float maxChangePropertyCoolTime = 35; //최대 속성 변경 시간
 
-    public float nowChangePropertyCoolTime; //현재 속성 변경 시간 !
+    private float nowChangePropertyCoolTime; //현재 속성 변경 시간
 
     public float NowChangePropertyCoolTime
     {
@@ -46,7 +46,7 @@ public class Player : BasicUnitScript
 
     private float maxPropertyTimeLimit = 25; //최대 속성 지속시간
 
-    public float nowPropertyTimeLimit; // 현재 속성 남은 지속시간 !
+    private float nowPropertyTimeLimit; // 현재 속성 남은 지속시간
 
     public float NowPropertyTimeLimit
     {
@@ -64,7 +64,7 @@ public class Player : BasicUnitScript
         }
     }
 
-    public NowProperty nowProperty; //!
+    private NowProperty nowProperty; 
 
     private int nextPropertyIndex;
 
